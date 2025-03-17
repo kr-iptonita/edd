699410930b5505b5d744e020539c88b1bd3c7493
kriptonita <kr.iptonita@outlook.com>
2025-03-17 10:31:47 -0600

    actualizaci{on de la matriz

--- gato/src/main/java/mx/unam/fciencias/edd/gato/modelo/Matriz.java
+++ gato/src/main/java/mx/unam/fciencias/edd/gato/modelo/Matriz.java
@@ -1,43 +1,43 @@
 package mx.unam.fciencias.edd.gato.modelo;
 
-public class Matriz {
-    private String[] celdas;
-    private String jugadorActual;
+public class Matriz {//Clase que contiene la matriz del juego
+    private String[] celdas;//Arreglo de celdas
+    private String jugadorActual;//Jugador actual
 
     public Matriz() {
-        celdas = new String[9];
+        celdas = new String[9];//Se crea un nuevo arreglo de celdas
         jugadorActual = "X";
         inicializarMatriz();
     }
 
     private void inicializarMatriz() {
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {//Se inicializa la matriz
             celdas[i] = String.valueOf(i + 1);
         }
     }
 
     public String[] getCeldas() {
-        return celdas;
+        return celdas;//Se obtienen las celdas
     }
 
     public String getJugadorActual() {
-        return jugadorActual;
+        return jugadorActual;//Se obtiene el jugador actual
     }
 
     public void setJugadorActual(String jugadorActual) {
-        this.jugadorActual = jugadorActual;
+        this.jugadorActual = jugadorActual;//Se cambia el jugador actual
     }
 
     public void realizarMovimiento(int posicion, String jugador) {
-        celdas[posicion] = jugador;
+        celdas[posicion] = jugador;//Se realiza un movimiento
     }
 
     public boolean esMovimientoValido(int posicion) {
-        return celdas[posicion].equals(String.valueOf(posicion + 1));
+        return celdas[posicion].equals(String.valueOf(posicion + 1));//Se verifica si el movimiento es válido
     }
 
-    public boolean hayGanador() {
-        int[][] combinaciones = {
+    public boolean hayGanador() {//Método que verifica si hay un ganador
+        int[][] combinaciones = {//
             {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Filas
             {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Columnas
             {0, 4, 8}, {2, 4, 6}            // Diagonales
@@ -48,8 +48,8 @@ public class Matriz {
                 celdas[combinacion[1]].equals(celdas[combinacion[2]])) {
                 return true;
             }
-        }
-        return false;
+        }//Se verifica si hay un ganador
+        return false;//Si no hay ganador
     }
 
     public boolean hayEmpate() {
@@ -57,7 +57,7 @@ public class Matriz {
             if (celda.matches("[1-9]")) {
                 return false;
             }
-        }
+        }//Se verifica si hay un empate
         return true;
     }
 }
